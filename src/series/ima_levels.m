b3238cba08e9d203f6193bd42b53f89f33ae5e54
Louis Gostiaux <Louis.Gostiaux@ec-lyon.fr>
2010-03-24 14:23:18 +0000

    ima_levels added to depository. This file used in series reequalizes the images before PIV proecssing

---
+++ src/series/ima_levels.m
@@ -0,0 +1,94 @@
+% relabel_i_j: relabel an image series with two indices, according to the time matrix given by ImaDoc
+%----------------------------------------------------------------------
+function GUI_input=ima_levels(num_i1,num_i2,num_j1,num_j2,Series)
+%requests for the visibility of input windows in the GUI series  (activated directly by the selection in the menu ACTION)
+if ~exist('num_i1','var')
+    GUI_input={};
+    return %exit the function 
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%enable waitbar
+hseries=guidata(Series.hseries);%handles of the GUI series
+WaitbarPos=get(hseries.waitbar_frame,'Position');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+basename=fullfile(Series.RootPath,Series.RootFile) ;
+
+%create dir of the new images
+[dir_images,namebase]=fileparts(basename);
+[path,subdir_ima]=fileparts(dir_images);
+curdir=pwd;
+cd(path);
+mkdir([subdir_ima '_levels']);
+cd(curdir);
+basename_new=fullfile(path,[subdir_ima '_levels'],namebase);
+
+% read imadoc
+[XmlData,warntext]=imadoc2struct([basename '.xml']);
+nbfield1=size(XmlData.Time,1)
+nbfield2=size(XmlData.Time,2)
+
+answer=msgbox_uvmat('CONFIRMATION','apply image rescaling function levels.m ')
+
+%copy the xml file
+if exist([basename '.xml'],'file')
+    copyfile([basename '.xml'],[basename_new '.xml']);% copy the .civ file
+end
+
+%main loop
+nbfield=size(num_i1,2)
+nbfield2=size(num_i1,1)
+for ifile=1:nbfield
+    update_waitbar(hseries.waitbar,WaitbarPos,ifile/nbfield)
+    stopstate=get(hseries.RUN,'BusyAction');
+    if isequal(stopstate,'queue') % enable STOP command
+        for jfile=1:nbfield2
+            filename=name_generator(basename,num_i1(jfile,ifile),num_j1(jfile,ifile),Series.FileExt,Series.NomType);
+            filename_new=name_generator(basename_new,num_i1(jfile,ifile),num_j1(jfile,ifile),'.png',Series.NomType);
+            A=imread(filename);
+            C=levels(A);
+            imwrite(C,filename_new)
+        end
+    end
+end
+
+
+
+function C=levels(A)
+%whos A;
+B=double(A(:,:,1));
+windowsize=round(min(size(B,1),size(B,2))/20);
+windowsize=floor(windowsize/2)*2+1;
+ix=[1/2-windowsize/2:-1/2+windowsize/2];%
+%del=np/3;
+%fct=exp(-(ix/del).^2);
+fct2=cos(ix/(windowsize-1)/2*pi/2);
+%Mfiltre=(ones(5,5)/5^2);
+%Mfiltre=fct2';
+Mfiltre=fct2'*fct2;
+Mfiltre=Mfiltre/(sum(sum(Mfiltre)));
+
+C=filter2(ones(windowsize)/windowsize^2,B);
+C(:,1:windowsize)=C(:,windowsize)*ones(1,windowsize);
+C(:,end-windowsize+1:end)=C(:,end-windowsize+1)*ones(1,windowsize);
+C(1:windowsize,:)=ones(windowsize,1)*C(windowsize,:);
+C(end-windowsize+1:end,:)=ones(windowsize,1)*C(end-windowsize,:);
+C=tanh(B./(2*C));
+[n,c]=hist(reshape(C,1,[]),100);
+% figure;plot(c,n);
+
+[m,i]=max(n);
+c_max=c(i);
+[dummy,index]=sort(abs(c-c(i)));
+n=n(index);
+c=c(index);
+i_select = find(cumsum(n)<0.95*sum(n));
+if isempty(i_select)
+    i_select = 1:length(c);
+end
+c_select=c(i_select);
+n_select=n(i_select);
+cmin=min(c_select);
+cmax=max(c_select);
+C=(C-cmin)/(cmax-cmin)*256;
+C=uint8(C);
