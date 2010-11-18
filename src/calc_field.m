ed433318e54b60c5ceb7b103ca3fa865e8717ea1
Louis Gostiaux <Louis.Gostiaux@ec-lyon.fr>
2010-11-18 21:55:22 +0000

    smart indent

--- src/calc_field.m
+++ src/calc_field.m
@@ -1,14 +1,14 @@
-%'calc_field': defines fields (velocity, vort, div...) from civx data and calculate them  
+%'calc_field': defines fields (velocity, vort, div...) from civx data and calculate them
 %---------------------------------------------------------------------
 % [DataOut,errormsg]=calc_field(FieldName,DataIn)
 %
-% OUTPUT: 
-% Scal: matlab vector representing the scalar values (length nbvec defined by var_read)  
+% OUTPUT:
+% Scal: matlab vector representing the scalar values (length nbvec defined by var_read)
 %      if no input, Scal=list of programmed scalar names (to put in menus)
 %      if only the field name is put as input, vec_A=type of scalar, which can be:
 %                   'discrete': related to the individual velocity vectors, not interpolated by patch
 %                   'vel': scalar calculated solely from velocity components
-%                   'der': needs spatial derivatives     
+%                   'der': needs spatial derivatives
 %                   'var': the scalar name directly corresponds to a field name in the netcdf files
 % error: error flag
 %      error = 0; OK
@@ -19,33 +19,34 @@
 % DataIn: structure representing the field, as defined in check_field_srtructure.m
 %
 % FUNCTION related
-% varname_generator.m: determines the field names to read in the netcdf file, depending on the scalar
+% varname_generator.m: determines the field names to read in the netcdf
+% file, depending on the scalar
 
 function [DataOut,errormsg]=calc_field(FieldName,DataIn)
 
 %list of defined scalars to display in menus (in addition to 'ima_cor').
-% a type is associated to each scalar: 
+% a type is associated to each scalar:
 %              'discrete': related to the individual velocity vectors, not interpolated by patch
-%              'vel': calculated from velocity components, continuous field (interpolated with velocity)              
-%              'der': needs spatial derivatives     
+%              'vel': calculated from velocity components, continuous field (interpolated with velocity)
+%              'der': needs spatial derivatives
 %              'var': the scalar name corresponds to a field name in the netcdf files
 % a specific variable name for civ1 and civ2 fields are also associated, if
 % the scalar is calculated from other fields, as explicited below
 
 %list_scal={title, type, civ1 var name,civ2 var name}
 list_field={'velocity';...%image correlation corresponding to a vel vector
-           'ima_cor';...%image correlation corresponding to a vel vector
-           'norm_vel';...%norm of the velocity
-           'vort';...%vorticity
-           'div';...%divergence
-           'strain';...%rate of strain
-           'u';... %u velocity component
-           'v';... %v velocity component
-           'w';... %w velocity component
-           'w_normal';... %w velocity component normal to the plane
-          'error'}; %error associated to a vector (for stereo or patch)
+    'ima_cor';...%image correlation corresponding to a vel vector
+    'norm_vel';...%norm of the velocity
+    'vort';...%vorticity
+    'div';...%divergence
+    'strain';...%rate of strain
+    'u';... %u velocity component
+    'v';... %v velocity component
+    'w';... %w velocity component
+    'w_normal';... %w velocity component normal to the plane
+    'error'}; %error associated to a vector (for stereo or patch)
 errormsg=[]; %default error message
-if ~exist('FieldName','var') 
+if ~exist('FieldName','var')
     DataOut=list_field;% gives the list of possible fields in the absence of input
 else
     if ~exist('DataIn','var')
@@ -58,24 +59,24 @@ else
         nbcoord=3;
     else
         nbcoord=2;
-    end    
+    end
     DataOut=DataIn; %reproduce global attribute
     ListVarName={};
     ValueList={};
     RoleList={};
     units_cell={};
-    for ilist=1:length(FieldName)  
+    for ilist=1:length(FieldName)
         [VarName,Value,Role,units]=feval(FieldName{ilist},DataIn);%calculate field with appropriate function named FieldName{ilist}
         ListVarName=[ListVarName VarName];
         ValueList=[ValueList Value];
         RoleList=[RoleList Role];
         units_cell=[units_cell units];
-   end   
-       %erase previous data (except coordinates)
+    end
+    %erase previous data (except coordinates)
     for ivar=nbcoord+1:length(DataOut.ListVarName)
         VarName=DataOut.ListVarName{ivar};
         DataOut=rmfield(DataOut,VarName);
-    end  
+    end
     DataOut.ListVarName=DataOut.ListVarName(1:nbcoord);
     if isfield(DataOut,'VarDimName')
         DataOut.VarDimName=DataOut.VarDimName(1:nbcoord);
@@ -87,8 +88,8 @@ else
     %append new data
     DataOut.ListVarName=[DataOut.ListVarName ListVarName];
     for ivar=1:length(ListVarName)
-        DataOut.VarDimName{nbcoord+ivar}=DataOut.VarDimName{1};      
-        DataOut.VarAttribute{nbcoord+ivar}.Role=RoleList{ivar};  
+        DataOut.VarDimName{nbcoord+ivar}=DataOut.VarDimName{1};
+        DataOut.VarAttribute{nbcoord+ivar}.Role=RoleList{ivar};
         DataOut.VarAttribute{nbcoord+ivar}.units=units_cell{ivar};
         eval(['DataOut.' ListVarName{ivar} '=ValueList{ivar};'])
     end
@@ -114,7 +115,7 @@ end
 if isfield(DataIn,'V')
     VarName=[VarName {'V'}];
     ValCell=[ValCell {DataIn.V}];
-    Role=[Role {'vector_y'}];  
+    Role=[Role {'vector_y'}];
     units_cell=[units_cell {units}];
 end
 if isfield(DataIn,'W')
@@ -157,18 +158,18 @@ Role={};
 units={};
 if isfield(DataIn,'U') && isfield(DataIn,'V')
     VarName{1}='norm_vel';
-     ValCell{1}=DataIn.U.*DataIn.U+ DataIn.V.*DataIn.V;
-     if isfield(DataIn,'W') && isequal(size(DataIn.W),size(DataIn.U))
-         ValCell{1}=ValCell{1}+DataIn.W.*DataIn.W;
-     end
-     ValCell{1}=sqrt(ValCell{1});
-     Role{1}='scalar';
-     if isfield(DataIn,'CoordUnit') && isfield(DataIn,'TimeUnit')
+    ValCell{1}=DataIn.U.*DataIn.U+ DataIn.V.*DataIn.V;
+    if isfield(DataIn,'W') && isequal(size(DataIn.W),size(DataIn.U))
+        ValCell{1}=ValCell{1}+DataIn.W.*DataIn.W;
+    end
+    ValCell{1}=sqrt(ValCell{1});
+    Role{1}='scalar';
+    if isfield(DataIn,'CoordUnit') && isfield(DataIn,'TimeUnit')
         units={[DataIn.CoordUnit '/' DataIn.TimeUnit]};
-     else
+    else
         units={'pixel'};
-     end
-end  
+    end
+end
 
 
 
@@ -189,7 +190,7 @@ if isfield(DataIn,'DjUi')
     else
         units={[]};
     end
-end  
+end
 
 %%%%%%%%%%%%% divergence%%%%%%%%%%%%%%%%%%%%
 function [VarName,ValCell,Role,units]=div(DataIn)
@@ -208,7 +209,7 @@ if isfield(DataIn,'DjUi')
     else
         units={[]};
     end
-end  
+end
 
 %%%%%%%%%%%%% strain %%%%%%%%%%%%%%%%%%%%
 function [VarName,ValCell,Role,units]=strain(DataIn)
@@ -217,16 +218,16 @@ ValCell={};
 Role={};
 units={};
 if isfield(DataIn,'DjUi')
-   VarName{1}='strain'; 
-   ValCell{1}=DataIn.DjUi(:,1,2)+DataIn.DjUi(:,2,1);%DVDX+DUDY
-   siz=size(ValCell{1});    
-   ValCell{1}=reshape(ValCell{1},siz(1),1); 
-   if isfield(DataIn,'TimeUnit')
+    VarName{1}='strain';
+    ValCell{1}=DataIn.DjUi(:,1,2)+DataIn.DjUi(:,2,1);%DVDX+DUDY
+    siz=size(ValCell{1});
+    ValCell{1}=reshape(ValCell{1},siz(1),1);
+    if isfield(DataIn,'TimeUnit')
         units={[DataIn.TimeUnit '-1']};
-   else
+    else
         units={[]};
-   end
-end  
+    end
+end
 
 %%%%%%%%%%%%% u %%%%%%%%%%%%%%%%%%%%
 function [VarName,ValCell,Role,units]=u(DataIn)
@@ -288,7 +289,7 @@ units={};
 if isfield(DataIn,'W')
     VarName{1}='W';
     ValCell{1}=DataIn.W;
-    Role{1}='vector_z';%will behave like a vector component  by projection 
+    Role{1}='vector_z';%will behave like a vector component  by projection
     if isfield(DataIn,'CoordUnit') && isfield(DataIn,'TimeUnit')
         units={[DataIn.CoordUnit '/' DataIn.TimeUnit]};
     else
