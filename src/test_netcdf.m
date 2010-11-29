71b228c92330cfd4902c2a952ce62a95924bc17b
Joel Sommeri <Joel.Sommeria@univ-grenoble-alpes.fr>
2010-11-29 09:03:11 +0000

    test_netcdf program created to test writting and reading netcdf files, replaces test_struct2nc

---
+++ src/test_netcdf.m
@@ -0,0 +1,61 @@
+% program to test struct2nc for creating netcdf files
+%% creating a simple netcdf file 
+Data.ListVarName={'A'};% matrix A to record
+Data.VarDimName={{'ny','nx'}};
+[x,y] = meshgrid(-2:.2:2);
+Data.A = x.*exp(-x.^2-y.^2);
+err=struct2nc('test_1.nc',Data);
+if isempty(err)
+    display('test_1.nc written')
+end
+%
+%% reading the simple netcdf file
+DataOut=nc2struct('test_1.nc');
+display( 'reading the simple netcdf file:')
+display(DataOut)
+
+%% writing a documented set of fields in a netcdf file
+Data.ListGlobalAttribute={'title','Time','TimeUnit'};
+Data.title='test field for netcdf';
+Data.Time=15;
+Data.TimeUnit='s';
+Data.ListVarName={'X','Y','U','V','coord_y','coord_x','A'};%  cell array containing the names of the fields to record
+Data.VarDimName={'nbvec','nbvec','nbvec','nbvec','coord_y','coord_x',{'coord_y','coord_x'}};
+Data.VarAttribute{1}.longname='abscissa';
+Data.VarAttribute{2}.longname='ordinate';
+Data.VarAttribute{3}.longname='x velocity component';
+Data.VarAttribute{4}.longname='y velocity component';
+Data.VarAttribute{5}.longname='interpolated abscissa';
+Data.VarAttribute{6}.longname='interpolated ordinate';
+Data.VarAttribute{7}.longname='stream function';
+
+nbvec=200;
+Data.X=4*rand(1,nbvec)-2;%random set of abscissa between -2 and 2
+Data.Y=4*rand(1,nbvec)-2;%random set of ordinates between -2 and 2
+Psi=exp(-Data.X.^2-Data.Y.^2);
+Data.U=Data.Y.*Psi;
+Data.V=-Data.X.*Psi;
+Data.coord_y=[-2 2];
+Data.coord_x=[-2 2];
+Data.A =exp(-x.^2-y.^2);
+err=struct2nc('test_2.nc',Data);
+if isempty(err)
+    display('test_2.nc written')
+else
+    msgbox_uvmat('ERROR',err)
+end
+
+%% fast reading of global attributes
+DataOut_global=nc2struct('test_2.nc','ListGlobalAttribute','title','Time');
+display('fast reading of global attributes:')
+display(DataOut_global)
+
+%% reading of the whole netcdf file
+DataOut_all=nc2struct('test_2.nc');
+display('reading of the whole netcdf file:')
+display(DataOut_all)
+
+%% reading only the field A, coord_y, coord_x  only
+DataOut_A=nc2struct('test_2.nc',{'coord_y','coord_x','A'}); 
+display('reading only the field A, coord_y, coord_x  only:')
+display(DataOut_A)
