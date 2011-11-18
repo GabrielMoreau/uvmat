3684c751413154c5da37e99b74e66d333f1a53a5
Joel Sommeri <Joel.Sommeria@univ-grenoble-alpes.fr>
2011-11-18 14:35:59 +0000

    further cleaning after new GUI civ. A new function read_GUI is introduced to get directely all the parameters from any GUI with appropriate conventions: num_...=tag of a numerical edit box. The default input value is given as UserData txt_...=tag for a text edit box

---
+++ src/read_GUI.m
@@ -0,0 +1,49 @@
+% --------------------------------------------------------------------
+% --- read a GUI with handle 'handle' producing a structure 'struct'
+function struct=read_GUI(handle)
+hchild=get(handle,'children');
+for ichild=1:numel(hchild)
+    if strcmp(get(hchild(ichild),'Visible'),'on')
+        object_type=get(hchild(ichild),'Type');
+        tag=get(hchild(ichild),'tag');
+        switch object_type
+            case 'uipanel'
+                eval(['struct.' tag '=read_GUI(hchild(ichild));'])
+            case 'uicontrol'
+                object_style=get(hchild(ichild),'Style');
+                check_input=1;%default
+                switch object_style
+                    case 'edit'
+                        separator=regexp(tag,'_');
+                        if isempty(separator)
+                            input=get(hchild(ichild),'Value');
+                        else
+                            switch(tag(1:separator))
+                                case 'num_'
+                                    input=str2double(get(hchild(ichild),'String'));
+                                    tag=tag(separator+1:end);
+                                    %deal with undefined input: retrieve the default value stored as UserData
+                                    if isnan(input)
+                                        input=get(hchild(ichild),'UserData');
+                                        set(hchild(ichild),'String',num2str(input))
+                                    end
+                                case 'txt_'
+                                    input=get(hchild(ichild),'String');
+                                    tag=tag(separator+1:end);
+                                otherwise
+                                    input=get(hchild(ichild),'String');
+                            end
+                        end
+                    case 'checkbox'
+                        input=get(hchild(ichild),'Value');
+                        %                         key=tag(7:end);
+                    otherwise
+                        check_input=0;
+                end
+                if check_input
+                    ['struct.' tag '=input;']
+                    eval(['struct.' tag '=input;'])
+                end
+        end
+    end
+end
