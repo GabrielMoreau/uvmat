cec31a7ea93bec06603e80e9f095de34146e23ad
Louis Gostiaux <Louis.Gostiaux@ec-lyon.fr>
2011-12-06 20:36:03 +0000

    added xml2struct.m

---
+++ src/xml2struct.m
@@ -0,0 +1,27 @@
+function s=xml2struct(filename)
+% structure parser, converts numeric character strings into numbers
+
+t=xmltree(filename);
+ss=convert(t)
+s=convert_string(ss);
+
+
+function out=convert_string(s)
+info=whos('s');
+switch info.class
+    case 'struct'
+        names = fieldnames(s);
+        for k=1:length(names)
+            out.(names{k})=convert_string(s.(names{k}));
+        end
+    case 'char'
+        if isempty(regexp(s,'\<\d+\>'))
+            out=s;
+        else
+            out=str2num(s);
+        end
+    otherwise
+        out=s;
+end
+
+    
