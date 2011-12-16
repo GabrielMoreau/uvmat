66f8de799dcaae36669273b826b0bcdef66572cc
Louis Gostiaux <Louis.Gostiaux@ec-lyon.fr>
2011-12-16 14:45:03 +0000

    Added the reverse_pair (need to uncomment line 57)

--- src/civ_matlab.m
+++ src/civ_matlab.m
@@ -53,7 +53,26 @@ end
 if isfield (Param,'Civ1')
     check_civ1=1;% test for further use of civ1 results
     % caluclate velocity data (y and v in indices, reverse to y component)
-    [xtable ytable utable vtable ctable F result_conv errormsg] = civ (Param.Civ1);
+    [xtable1 ytable1 utable1 vtable1 ctable1 F1 result_conv1 errormsg1] = civ (Param.Civ1);
+
+    % to try the reverse_pair method, uncomment below
+%     Param.Civ1.reverse_pair=1;
+%     [xtable2 ytable2 utable2 vtable2 ctable2 F2 result_conv2 errormsg2] = civ (Param.Civ1);
+
+    xtable=[xtable1; xtable2];
+    ytable=[ytable1; ytable2];
+    utable=[utable1; -utable2];
+    vtable=[vtable1; -vtable2];
+    ctable=[ctable1; ctable2];
+    F=[F1; F2];
+    result_conv=[result_conv1; result_conv2];
+    errormsg=[errormsg1; errormsg2];
+    
+%     xtable=mean(xtable1,xtable2);
+%     ytable=mean(ytable1,ytable2);
+%     utable=mean(utable1,utable2);
+%     vtable=mean(vtable1,vtable2);    
+    
     if ~isempty(errormsg)
         return
     end
@@ -388,12 +407,25 @@ check_MinIma=isfield(par_civ,'MinIma');% test for image luminosity threshold
 check_MaxIma=isfield(par_civ,'MaxIma') && ~isempty(par_civ.MaxIma);
 
 %% prepare images
-if ischar(par_civ.ImageA)
-    par_civ.ImageA=imread(par_civ.ImageA);
+if isfield(par_civ,'reverse_pair')
+    if par_civ.reverse_pair
+        if ischar(par_civ.ImageB)
+            temp=par_civ.ImageA;
+            par_civ.ImageA=imread(par_civ.ImageB);
+        end
+        if ischar(temp)
+            par_civ.ImageB=imread(temp);
+        end
+    end
+else
+    if ischar(par_civ.ImageA)
+        par_civ.ImageA=imread(par_civ.ImageA);
+    end
+    if ischar(par_civ.ImageB)
+        par_civ.ImageB=imread(par_civ.ImageB);
+    end
 end
-if ischar(par_civ.ImageB)
-    par_civ.ImageB=imread(par_civ.ImageB);
-end
+
 [npy_ima npx_ima]=size(par_civ.ImageA);
 if ~isequal(size(par_civ.ImageB),[npy_ima npx_ima])
     errormsg='image pair with unequal size';
@@ -502,7 +534,7 @@ F=0;
 [npy,npx]=size(result_conv);
 
 % if (x <= (size(result_conv,1)-1)) && (y <= (size(result_conv,1)-1)) && (x >= 1) && (y >= 1)
-    %the following 8 lines are copyright (c) 1998, Uri Shavit, Roi Gurka, Alex Liberzon, Technion – Israel Institute of Technology
+    %the following 8 lines are copyright (c) 1998, Uri Shavit, Roi Gurka, Alex Liberzon, Technion ï¿½ Israel Institute of Technology
     %http://urapiv.wordpress.com
     peaky = y;
     if y <= npy-1 && y >= 1
@@ -538,11 +570,11 @@ if (x <= npx-1) && (y <= npy-1) && (x >= 1) && (y >= 1)
     for i=-1:1
         for j=-1:1
             %following 15 lines based on
-            %H. Nobach Æ M. Honkanen (2005)
+            %H. Nobach ï¿½ M. Honkanen (2005)
             %Two-dimensional Gaussian regression for sub-pixel displacement
             %estimation in particle image velocimetry or particle position
             %estimation in particle tracking velocimetry
-            %Experiments in Fluids (2005) 38: 511–515
+            %Experiments in Fluids (2005) 38: 511ï¿½515
             c10(j+2,i+2)=i*log(result_conv(y+j, x+i));
             c01(j+2,i+2)=j*log(result_conv(y+j, x+i));
             c11(j+2,i+2)=i*j*log(result_conv(y+j, x+i));
