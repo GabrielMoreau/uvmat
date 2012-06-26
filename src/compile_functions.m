2f5da6fe3254c710ab11606170f0ca9cb66b80b1
Louis Gostiaux <Louis.Gostiaux@ec-lyon.fr>
2012-06-26 13:18:55 +0000

    .sh files in the uvmat folder, binary in bin subfolder

--- src/compile_functions.m
+++ src/compile_functions.m
@@ -1,8 +1,11 @@
-% commands to 
+% commands to compile civ_matlab and eventually other functions
 
 mcc -m -R -nojvm -R -nodisplay civ_matlab.m
-system('mv civ_matlab bin/');
-system('sed -e ''s#/civ_matlab#/bin/civ_matlab#'' run_civ_matlab.sh > run_civ_matlab.sh.correct'); 
-system('mv -f run_civ_matlab.sh.correct  run_civ_matlab.sh');
+system('mv -f civ_matlab bin/');
+system('sed -e ''s#/civ_matlab#/bin/civ_matlab#'' run_civ_matlab.sh > civ_matlab.sh'); 
+system('rm run_civ_matlab.sh');
+system('chmod +x civ_matlab.sh');
+
+
 
 
