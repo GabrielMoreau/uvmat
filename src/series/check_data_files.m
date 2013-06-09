2d132ad33ed25e1fdbd113abd492bb005dae5487
Joel Sommeri <Joel.Sommeria@univ-grenoble-alpes.fr>
2013-06-09 20:32:50 +0000

    open_uvmat integrated in check_data_files

--- src/series/check_data_files.m
+++ src/series/check_data_files.m
@@ -176,3 +176,26 @@ for iview=1:nbview
     h=uicontrol('Style','listbox', 'Position', [20 20 500 300], 'String', Tabchar, 'Callback', {'open_uvmat'});
     hh=uicontrol('Style','listbox', 'Position', [20 340 500 40], 'String', message);
 end
+
+% 'open_uvmat': open with uvmat the  field selected in the list of 'series/check_data_files'
+%------------------------------------------------------------------------
+%function open_uvmat(hObject, eventdata)
+%
+% INPUT: 
+% hObject: handle of uicontrol object containing the list 
+% eventdata: not used
+function open_uvmat(hObject, eventdata)
+%------------------------------------------------------------------------
+list=get(hObject,'String');
+index=get(hObject,'Value');
+rootroot=get(hObject,'UserData');
+filename=list{index};
+ind_dot=strfind(filename,'...');
+if ~isempty(ind_dot)
+filename=filename(1:ind_dot-1);
+end
+filename=fullfile(rootroot,filename);
+if exist(filename,'file')%visualise the vel field if it exists
+    uvmat(filename)
+    set(gcbo,'Value',1)
+end
