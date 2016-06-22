15b4838b9e56ac84c4916284e2584c0b6d23266e
Joel Sommeri <Joel.Sommeria@univ-grenoble-alpes.fr>
2016-06-22 18:27:38 +0000

    ima_filter restored

--- src/transform_field/ima_filter.m
+++ src/transform_field/ima_filter.m
@@ -28,15 +28,29 @@
 %     GNU General Public License (see LICENSE.txt) for more details.
 %=======================================================================
 
-function DataOut=ima_filter(DataIn)
+function DataOut=ima_filter(DataIn,Param)
 
-Origin_x=469;
+%% request input parameters
+if isfield(DataIn,'Action') && isfield(DataIn.Action,'RUN') && isequal(DataIn.Action.RUN,0)
+    prompt = {'npx';'npy'};
+    dlg_title = 'get the filter size in x and y';
+    num_lines= 2;
+    def     = { '20';'20'};
+    if isfield(Param,'TransformInput')&&isfield(Param.TransformInput,'FilterBoxSize_x')&&...
+            isfield(Param.TransformInput,'FilterBoxSize_y')
+        def={num2str(Param.TransformInput.FilterBoxSize_x);num2str(Param.TransformInput.FilterBoxSize_y)};
+    end
+    answer = inputdlg(prompt,dlg_title,num_lines,def);
+    DataOut.TransformInput.FilterBoxSize_x=str2num(answer{1}); %size of the filtering window
+    DataOut.TransformInput.FilterBoxSize_y=str2num(answer{2}); %size of the filtering window
+    return
+end
+
 DataOut=DataIn; %default
-DataOut.A=
 
 %definition of the cos shape matrix filter
-ix=[1/2-Param.TransformInput.FilterBoxSize_x/2:-1/2+Param.TransformInput.FilterBoxSize_x/2];%
-iy=[1/2-Param.TransformInput.FilterBoxSize_y/2:-1/2+Param.TransformInput.FilterBoxSize_y/2];%
+ix=1/2-Param.TransformInput.FilterBoxSize_x/2:-1/2+Param.TransformInput.FilterBoxSize_x/2;%
+iy=1/2-Param.TransformInput.FilterBoxSize_y/2:-1/2+Param.TransformInput.FilterBoxSize_y/2;%
 %del=np/3;
 %fct=exp(-(ix/del).^2);
 fct2_x=cos(ix/((Param.TransformInput.FilterBoxSize_x-1)/2)*pi/2);
@@ -50,7 +64,7 @@ if numel(size(DataIn.A))==3
     DataOut.A=filter2(Mfiltre,sum(DataIn.A,3));%filter the input image, after summation on the color component (for color images)
     DataOut.A=uint16(DataOut.A); %transform to 16 bit images
 else
-    DataOut.A=filter2(Mfiltre,DataIn.A)
+    DataOut.A=filter2(Mfiltre,DataIn.A);
     DataOut.A=feval(Atype,DataOut.A);%transform to the initial image format
 end
  
