13aeca12b4dd1589ee03bd8d6d4eb671f3e2f25f
Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
2018-05-10 12:35:36 +0000

    * Just nice format

--- src/cluster_command_psmn.m
+++ src/cluster_command_psmn.m
@@ -16,13 +16,13 @@
 
 function cmd=cluster_command_psmn(ListProcessFile, ActionFullName, DirLog, NbProcess, NbCore, CPUTimeProcess)
     SubmitScriptFile = regexprep(ListProcessFile, '\job_list.txt\>', 'submit_script.sh');
-	fid_list = fopen(ListProcessFile, 'r');
+    fid_list = fopen(ListProcessFile, 'r');
     fid_submit = fopen(SubmitScriptFile, 'w');
     i=1;
-	while(true)
-		process = fgets(fid_list);
+    while(true)
+        process = fgets(fid_list);
         if(process == -1)
-			break
+            break
         end
         n = numel(process);
         process = process(1:(n-1)); % on enlève le trailing \n
@@ -34,8 +34,8 @@ function cmd=cluster_command_psmn(ListProcessFile, ActionFullName, DirLog, NbPro
                             '-N UVmat_' num2str(i) ' '...
                             process char(10)]);
         i=i+1;
-	end
-	fclose(fid_list);
+    end
+    fclose(fid_list);
     fclose(fid_submit);
     system(['chmod +x ' SubmitScriptFile]);
     cmd = SubmitScriptFile;
